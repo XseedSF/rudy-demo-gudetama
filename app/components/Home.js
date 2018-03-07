2ff809af25822892f4cf6ce3cda1e1b3486ef294
Federico Sobral <fsobral@xseed.com.uy>
2018-03-07 13:53:44 -0300

    fix typos, add titles to code snippets

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,13 +3,20 @@ import SyntaxHighlighter from 'react-syntax-highlighter/prism';
 import { dark } from 'react-syntax-highlighter/styles/prism';
 
 const Home = (props) => (
-  <div>
-    <p className='bold'>Welcome to this redux-first-router@rudy demo.</p>
+  <div className='home'>
+    <p className='bold'>redux-first-router@rudy Demo</p>
+    <section>
+    <p>Welcome to this redux-first-router@rudy demo.</p>
     <p>The purpose of this site is to test differents ways to perform the routing of a React/Redux application using the package mentioned above.</p>
-    <p>Everything else is just for fun (⊃◜⌓◝⊂)</p>
-    <div className='code-snippet'>
+    </section>
+    <section>
+    <p>In order to make this POC more fun the site is themed after Gudetama, a cute japanese character (⊃◜⌓◝⊂)</p>
+    </section>
+    <section>
+    <div className='code-snippet'>    
+    <p className='bold'>Navigation</p>
     <SyntaxHighlighter language='javascript' style={dark}>{
-         "// Links can be used providing a realtive path"
+         "// Links can be used providing a relative path"
        + "\n// this allows the use of SEO links"
        + "\n<Link to='/'>Home</Link>"
        + "\n<Link to='/smartphone'>Smartphone</Link>"
@@ -21,7 +28,17 @@ const Home = (props) => (
        + "\nconst mapDispatchToProps = dispatch => ({"
        + "\n  redirectToBacon3: () => dispatch({type: routerTypes.BACON, payload: { id: 3 }}),"
        + "\n});"
+       + "\n"
+       + "\nconst { "
+       + "\n	reducer,"
+       + "\n	middleware,"
+       + "\n	enhancer"
+       + "\n} = connectRoutes(routesMap, config);"
     }</SyntaxHighlighter>
+    </div>
+    <div className='code-snippet'>    
+    <p className='bold'>Router</p>
+    <p className='bold sub'>Reducer</p>
      <SyntaxHighlighter language='javascript' style={dark}>{
           "const routesMap = {"
         + "\n  [types.HOME]: { path: '/' },"
@@ -31,7 +48,7 @@ const Home = (props) => (
         + "\n}"
         + "\n"
         + "\nconst operations = {"
-        + "\n  // Inside the thunk we can wait for async operations and dispatch side effects"
+        + "\n  // Inside the thunk we can perform async operations and dispatch side effects"
         + "\n  smartphone: async (dispatch, getState) => {"
         + "\n    const ok = await sideEffect();"
         + "\n    // the actions to dispatch doesn't need to have a path"
@@ -39,7 +56,21 @@ const Home = (props) => (
         + "\n  },"
         + "\n}"
     }</SyntaxHighlighter>
+    <p className='bold sub'>Component</p>    
+    <SyntaxHighlighter language='javascript' style={dark}>{
+        + "const pagesMap = {"
+        + "\n  [routerTypes.HOME]: HomeContainer,"
+        + "\n  [routerTypes.SMARTPHONE]: ImageContainer,"
+        + "\n  [routerTypes.BACON]: ImageContainer,"
+        + "\n}"
+        + "\n"
+        + "\nconst PageComponent = (routerType = routerTypes.HOME) => {"
+        + "\n  const Page = pagesMap[routerType]"
+        + "\n  return Page ? <Page /> : null"
+        + "\n}"
+    }</SyntaxHighlighter>
     </div>
+    </section>
   </div>
 )
 
